4adb02148249f1e99b3618c01943df7a03b2926b
its-shadow7 <gusainpradeep57@gmail.com>
2026-06-19 11:31:10 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,31 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   while (true) {
     std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
-    std::string input = command;
-    if (input.substr(0, 4) == "exit") {
-    break;
-    }
-    else if (input.substr(0, 5) == "echo ") {
-        std::cout << input.substr(5) << std::endl;
-    }
-    else {
-        std::cout << command << ": command not found" << std::endl;
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::string command = input.substr(0, input.find(' '));
+    std::string parameters;
+
+    const std::size_t parameterIndex = input.find(' ') + 1;
+    if (parameterIndex != std::string::npos) {
+      parameters = input.substr(parameterIndex);
     }
 
+    if (command == "exit")
+      break;
 
+    if (command == "echo") {
+      std::cout << parameters << std::endl;
+    } else if (command == "type") {
+      if (parameters == "echo" || parameters == "exit" || parameters == "type")
+        std::cout << parameters << " is a shell builtin" << std::endl;
+      else
+        std::cout << parameters << ": not found" << std::endl;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
 }
